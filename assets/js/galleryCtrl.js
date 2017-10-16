4b4baffe2211e99b6aeda00868c8a1a3e055587d
karthikeyankkn <karthikeyan@fixnix.co>
2017-10-16 19:48:45 +0530

    filter added

--- assets/js/galleryCtrl.js
+++ assets/js/galleryCtrl.js
@@ -1,16 +1,15 @@
 var app = angular.module('smilefreeApp', []);
-	// reverse custom filter
-  	app.filter('reverse', function() {
-	  return function(items) {
-	    return items.slice().reverse();
-	  };
-	});
+
 	app.controller('galleryCtrl', function($scope, $http) {
+		var vm = this;
 		// debugger
+		$scope.kkn = function(search){
+	  	$scope.search = search;
+	  };
 		// get service
 	  $http.get("/assets/documents/gallery.json")
 	  .then(function(response) {
 	  	// debugger
 	      $scope.myWelcome = response.data;
-	  });
+	  }); 
 	}); 
